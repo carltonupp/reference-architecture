f215c4f6c84cac3093c6f5f7ae3a8df3b026cb9e
carltonupperdine <carlton.upperdine@gmail.com>
2021-11-17 20:15:38 +0000

    scaffold base architecture

---
+++ src/Products/Products.Service/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Products.Service.Controllers
+{
+    [ApiController]
+    [Route("/api/products")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
