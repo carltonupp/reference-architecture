078be81876335791eebae5f1ecb05d158da3b46e
carltonupperdine <carlton.upperdine@gmail.com>
2021-11-19 21:53:34 +0000

    Add Product constructor logic and tests

---
+++ src/Products/Products.Domain.Tests/Products/WhenCreatingProduct.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Products.Domain.Products;
+using Xunit;
+
+namespace Products.Domain.Tests.Products
+{
+    public class WhenCreatingProduct
+    {
+        [Fact]
+        public void CreatesSuccessfullyGivenValidParameters()
+        {
+            var product = new Product(
+                "Halfords Essentials Downtube Mudguard 2018",
+                "The Halfords Essentials Downtube Mudguard  is for 20\" to 27.5\" wheels and fits by straps to the downtube of the frame. We also offer a wide selection of other cycle parts. Halfords offer a fantastic range of Bike Parts and Bike Accessories at competitive prices!",
+                "537156", Guid.NewGuid(), 2.50m, 5.00m);
+            Assert.NotNull(product);
+        }
+
+        [Fact]
+        public void ThrowsExceptionGivenEmptyName()
+        {
+            Assert.Throws<Exception>(() =>
+            {
+                var product = new Product(
+                    "",
+                    "The Halfords Essentials Downtube Mudguard  is for 20\" to 27.5\" wheels and fits by straps to the downtube of the frame. We also offer a wide selection of other cycle parts. Halfords offer a fantastic range of Bike Parts and Bike Accessories at competitive prices!",
+                    "537156", Guid.NewGuid(), 2.50m, 5.00m);
+            });
+        }
+
+        [Fact]
+        public void ThrowsExceptionGivenEmptyDescription()
+        {
+            Assert.Throws<Exception>(() =>
+            {
+                var product = new Product(
+                    "Halfords Essentials Downtube Mudguard 2018",
+                    "",
+                    "537156", Guid.NewGuid(), 2.50m, 5.00m);
+            });
+        }
+
+        [Fact]
+        public void ThrowsExceptionGivenEmptyProductCode()
+        {
+            Assert.Throws<Exception>(() =>
+            {
+                var product = new Product(
+                    "Halfords Essentials Downtube Mudguard 2018",
+                    "The Halfords Essentials Downtube Mudguard  is for 20\" to 27.5\" wheels and fits by straps to the downtube of the frame. We also offer a wide selection of other cycle parts. Halfords offer a fantastic range of Bike Parts and Bike Accessories at competitive prices!",
+                    "", Guid.NewGuid(), 2.50m, 5.00m);
+            });
+        }
+
+        [Fact]
+        public void ThrowsExceptionGivenEmptyCategoryId()
+        {
+            Assert.Throws<Exception>(() =>
+            {
+                var product = new Product(
+                    "Halfords Essentials Downtube Mudguard 2018",
+                    "The Halfords Essentials Downtube Mudguard  is for 20\" to 27.5\" wheels and fits by straps to the downtube of the frame. We also offer a wide selection of other cycle parts. Halfords offer a fantastic range of Bike Parts and Bike Accessories at competitive prices!",
+                    "537156", Guid.Empty, 2.50m, 5.00m);
+            });
+        }
+
+        [Fact]
+        public void ThrowsExceptionGivenSellLessThanCost()
+        {
+            Assert.Throws<Exception>(() =>
+            {
+                var product = new Product(
+                    "Halfords Essentials Downtube Mudguard 2018",
+                    "The Halfords Essentials Downtube Mudguard  is for 20\" to 27.5\" wheels and fits by straps to the downtube of the frame. We also offer a wide selection of other cycle parts. Halfords offer a fantastic range of Bike Parts and Bike Accessories at competitive prices!",
+                    "537156", Guid.NewGuid(), 2.50m, 2.00m);
+            });
+        }
+    }
+}
---
+++ src/Products/Products.Domain/Products/Product.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Products.Domain.Products
+{
+    public class Product
+    {
+        private string _name;
+        private string _description;
+        private string _productCode;
+        private Guid _category;
+        private decimal _sell;
+
+        public Product(string name, string description, string productCode, Guid category, decimal cost, decimal sell)
+        {
+            Name = name;
+            Description = description;
+            ProductCode = productCode;
+            Category = category;
+            Cost = cost;
+            Sell = sell;
+        }
+
+        public Guid ProductId { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new Exception("Product name cannot be empty");
+
+                _name = value;
+            }
+        }
+
+        public string Description
+        {
+            get => _description;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new Exception("Product description cannot be empty");
+
+                _description = value;
+            }
+        }
+
+        public string ProductCode
+        {
+            get => _productCode;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new Exception("Product Code cannot be empty");
+
+                _productCode = value;
+            }
+        }
+
+        public Guid Category
+        {
+            get => _category;
+            private set
+            {
+                if (value == Guid.Empty)
+                    throw new Exception("Category ID cannot be empty");
+
+                _category = value;
+            }
+        }
+
+        public decimal Cost { get; private set; }
+
+        public decimal Sell
+        {
+            get => _sell;
+            private set
+            {
+                if (value < Cost)
+                    throw new Exception("Sell Price cannot be less than cost");
+
+                _sell = value;
+            }
+        }
+    }
+}
