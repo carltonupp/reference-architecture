8a27199384f4d36080ac345404ed7223a576912f
carltonupperdine <carlton.upperdine@gmail.com>
2021-11-21 14:18:28 +0000

    add logging to controller

--- src/Products/Products.Service/Controllers/ProductsController.cs
+++ src/Products/Products.Service/Controllers/ProductsController.cs
@@ -3,28 +3,31 @@ using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Products.Application.Products.Commands.CreateProduct;
 
 namespace Products.Service.Controllers
 {
     [ApiController]
-    [Route("/api/products")]
+    [Route("/api/[controller]")]
     public class ProductsController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly ILogger<ProductsController> _logger;
 
-        public ProductsController(IMediator mediator)
+        public ProductsController(IMediator mediator, ILogger<ProductsController> logger)
         {
             _mediator = mediator;
+            _logger = logger;
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public IActionResult Get()
         {
             return Ok();
         }
 
-        [HttpPost("/create")]
+        [HttpPost]
         [ProducesResponseType(typeof(ProductCreatedResponse), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Create(CreateProductCommand request)
@@ -36,9 +39,9 @@ namespace Products.Service.Controllers
             }
             catch (Exception ex)
             {
+                _logger.LogError("Product creation failed: {ex.Message}", ex);
                 return BadRequest(ex.Message);
             }
-
         }
     }
 }
