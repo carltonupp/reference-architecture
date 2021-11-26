32f3b12572cb73d1e3934e04bdc8f551e671ed72
Carlton Upperdine <carlton.upperdine@gmail.com>
2021-11-26 20:57:55 +0000

    remove category from product - too early

--- src/Products/Products.Domain.Tests/Fixtures/ProductFixture.cs
+++ src/Products/Products.Domain.Tests/Fixtures/ProductFixture.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Products.Domain.Products;
+﻿using Products.Domain.Products;
 
 namespace Products.Domain.Tests.Fixtures
 {
@@ -12,7 +11,7 @@ namespace Products.Domain.Tests.Fixtures
             Product = new Product(
                 "Halfords Essentials Downtube Mudguard 2018",
                 "The Halfords Essentials Downtube Mudguard  is for 20\" to 27.5\" wheels and fits by straps to the downtube of the frame. We also offer a wide selection of other cycle parts. Halfords offer a fantastic range of Bike Parts and Bike Accessories at competitive prices!",
-                "537156", Guid.NewGuid(), 2.50m, 5.00m);
+                "537156", 2.50m, 5.00m);
         }
     }
 }
