5e86b84056af39d81a0c5f98b5267dde6e3b3813
carltonupperdine <carlton.upperdine@gmail.com>
2022-01-17 19:17:32 +0000

    apply files

--- src/Products/Products.Application/Categories/Commands/CreateCategory/CreateCategoryCommand.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-
-namespace Products.Application.Categories.Commands.CreateCategory
-{
-    public class CreateCategoryCommand : IRequest<CategoryCreatedResponse>
-    {
-        public string Name { get; set; }
-    }
-}
